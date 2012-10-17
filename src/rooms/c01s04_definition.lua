a08f5b36917362c3908ada6564180b426e0b0d7e
Pablo del Sel <pablodelsel@gmail.com>
2012-10-17 18:49:44 -0300

    * Scene04 svg

---
+++ src/rooms/c01s04_definition.lua
@@ -0,0 +1,33 @@
+objects = {
+  
+  c01s04_building = {
+    layer_name = 'background',
+    type = RESOURCE_TYPE_IMAGE,
+    x = 0.0, y = 0.0,
+    width = 1920, height = 1280,
+    resource_name = 'c01s04/c01s04_building.png',
+    render_at_start = true,
+    renderPriority = 1,
+  },
+  
+  c01s04_edificio_costado = {
+    layer_name = 'background',
+    type = RESOURCE_TYPE_IMAGE,
+    x = 876.0832300000001, y = 0.0,
+    width = 167.83366, height = 1280,
+    resource_name = 'c01s04/c01s04_edificio_costado.png',
+    render_at_start = true,
+    renderPriority = 11,
+  },
+  
+  c01s04_walk-behind = {
+    layer_name = 'walk_behind',
+    type = RESOURCE_TYPE_IMAGE,
+    x = -226.43431999999996, y = 0.0,
+    width = 795.99866, height = 1280,
+    resource_name = 'c01s04/c01s04_walk-behind.png',
+    render_at_start = true,
+    renderPriority = 21,
+  },
+  
+}
